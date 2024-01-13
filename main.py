2216ab7c16322e0fdd1b6243e04b3ca8350f95d5
hamzaehsan03 <153811806+hamzaehsan03@users.noreply.github.com>
2024-01-13 02:08:14 +0000

    added automation for processing image dataset

--- main.py
+++ main.py
@@ -3,3 +3,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 from preprocess_pipeline import preprocess_image
+
+
+def process_images(image_dir):
+    for sub_directory, directory, images in os.walk(image_dir):
+        for image in images:
+            if image.endswith('.png'):
+                image_path = os.path.join(sub_directory, image)
+                print("processing", image_path)
+                preprocessed_image = preprocess_image(image_path)
+
+if __name__ == '__main__':
+    current_directory = os.getcwd()
+    image_directory = os.path.join(current_directory, "DeepLesion")
+    process_images(image_directory)
