664fef7a257b8c7ecdfa6b62e0aa52c5643a8274
hamzaehsan03 <153811806+hamzaehsan03@users.noreply.github.com>
2024-01-18 08:09:40 +0000

    changed split to 70/15/15 generalised working directories

--- image_split.py
+++ image_split.py
@@ -15,21 +15,21 @@ def parallel_copy(files, src_dir, dst_dir):
     
     arguments = [((os.path.join(src_dir, f), os.path.join(dst_dir, f)),) for f in files]
     parallel_process(copy_images, arguments)
-    
+
 
 def main():
 
-    base_dir = r"C:\\Users\\Hamza\\Documents\\University\\Dissertation stuff\\Code\\final-year-project\\ProcessedImages"
-    hr_dir = os.path.join(base_dir, "HR")
-    lr_dir = os.path.join(base_dir, "LR")
+    current_directory = os.cwd()
+    hr_dir = os.path.join(current_directory, "ProcessedImages\\HR")
+    lr_dir = os.path.join(current_directory, "ProcessedImages\\LR")
 
     hr_images = [os.path.join(hr_dir, f) for f in os.listdir(hr_dir) if f.endswith('.png')]
 
     # Split the dataset into training, validation, and test sets
-    train_files, test_files = train_test_split(hr_images, test_size=0.20, random_state=727) # wysi
-    train_files, val_files = train_test_split(train_files, test_size=0.25, random_state=727)  # Splitting the remaining 80% into 60% train and 20% val
+    train_files, test_files = train_test_split(hr_images, test_size=0.15, random_state=727) # wysi
+    train_files, val_files = train_test_split(train_files, test_size=0.1765, random_state=727)  # 70/15/15 split
 
-    for file_set, set_name in zip([train_files, val_files, test_files], ['Train', 'Val', 'Test']):
+    for file_set, set_name in zip([train_files, val_files, test_files], ['Train', 'Validation', 'Test']):
         hr_set_dir = os.path.join(hr_dir, set_name)
         lr_set_dir = os.path.join(lr_dir, set_name)
         
