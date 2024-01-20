7508edb8b9308f66a4394c390f2d53edd31e64ee
hamzaehsan03 <153811806+hamzaehsan03@users.noreply.github.com>
2024-01-20 20:58:33 +0000

    implemented vgg19 added some more loss function boiler plate stuff will probably remove vgg19 due to computational cost also made the threads persistent deleted some comments as well, i'll just go to commit history if i need it

--- model.py
+++ model.py
@@ -5,6 +5,7 @@ from torchvision import transforms, models
 from torch.utils.data import Dataset, DataLoader
 from torch.cuda.amp import GradScaler, autocast
 from torch.optim.lr_scheduler import StepLR, ReduceLROnPlateau
+from torch.nn import functional
 import torch.nn as nn
 import torch.optim as optim
 from PIL import Image
@@ -104,12 +105,31 @@ class PerceptualLoss(nn.Module):
     def __init__(self, feature_extractor):
         super(PerceptualLoss, self).__init__()
         self.feature_extractor = feature_extractor
-    
-    def forward(self, resolved_images, hr_images):
-        resolved_features = self.feature_extractor(resolved_images)
-        hr_features = self.feature_extractor(hr_images)
-        loss = nn.functional.mse_loss(resolved_features, hr_features)
-        return loss
+        for param in self.feature_extractor.parameters():
+            param.requires_grad = False
+
+    def forward(self, input, target):
+        input_features = self.feature_extractor(input)
+        target_features = self.feature_extractor(target)
+        return nn.functional.mse_loss(input_features, target_features)
+
+
+
+# class EdgeLoss(nn.Module):
+#     def __init__(self):
+#         super(EdgeLoss, self).__init__()
+#         self.sobel = nn.Sobel()
+#     def forward(self, input, target):
+#         input_edges = self.sobel(input)
+#         target_edges = self.sobel(target)
+#         return functional.mse_loss(input_edges, target_edges)
+
+#TD: IMPLEMENT AN SSIM FUNCTION
+
+# class SSIMLoss(torch.SSIM):
+#     def forward(self, input, target):
+#         return 1 - super(SSIMLoss, self).forward(input, target)
+
 
 
 def main():
@@ -146,7 +166,7 @@ def main():
     val_lr_dir = os.path.join(current_directory, "ProcessedImages\\LR\\Validation")
 
     train_dataset = SISRDataSet(hr_dir=hr_dir, lr_dir=lr_dir, training=True)
-    train_load = DataLoader(train_dataset, batch_size=16, shuffle=False, num_workers=12)
+    train_load = DataLoader(train_dataset, batch_size=16, shuffle=False, num_workers=12, persistent_workers=True)
 
     validation_dataset = SISRDataSet(hr_dir=val_hr_dir, lr_dir=val_lr_dir, transform=transforms.ToTensor(), training=False)
     validation_load = DataLoader(validation_dataset, batch_size=16, shuffle=False, num_workers=12)
@@ -157,23 +177,25 @@ def main():
     #scheduler = StepLR(optimiser, step_size=10, gamma=0.1)
     model = model.to(device)
 
-    # Convert VGG19 to use 1 layer instead of RGB
-    '''
-    model_vgg19 = models.vgg19(pretrained=True).features 
-    first_conv_layer = model_vgg19[0]
-    model_vgg19[0] = nn.Conv2d(1, first_conv_layer.out_channels, 
+    mse_loss_fn = nn.MSELoss()
+    vgg19 = models.vgg19(pretrained=True).features[:10].eval()  
+    first_conv_layer = vgg19[0]
+    new_first_layer = nn.Conv2d(1, first_conv_layer.out_channels, 
                             kernel_size=first_conv_layer.kernel_size, 
                             stride=first_conv_layer.stride, 
                             padding=first_conv_layer.padding)
-    model_vgg19 = model_vgg19.to(device)
-    model_vgg19.eval()
 
-    perceptual_loss_fn = PerceptualLoss(model_vgg19)
-   
-    '''
-    mse_loss_fn = nn.MSELoss()
-    alpha = 1.0
-    beta = 0.001
+# Replace the first layer
+    vgg19[0] = new_first_layer
+    vgg19 = vgg19.to('cuda' if torch.cuda.is_available() else 'cpu')  
+    perceptual_loss_fn = PerceptualLoss(vgg19)
+    #edge_loss_fn = EdgeLoss()
+    #ssim_loss_fn = SSIMLoss()
+    
+    lambda_mse = 1.0
+    lambda_perceptual = 0.1
+    lambda_edge = 0.1
+    lambda_ssim = 0.1
 
     num_epochs = 10 
     print_every_n_batches = 100  # Print information every n batches
@@ -189,11 +211,15 @@ def main():
         for batch_idx, (hr_images, lr_images) in enumerate(train_load):
             lr_images = lr_images.to(device)
             hr_images = hr_images.to(device)
+
             with autocast():
 
                 sr_images = model(lr_images)
                 mse_loss = mse_loss_fn(sr_images, hr_images)
-                total_loss = mse_loss
+                perceptual_loss = perceptual_loss_fn(sr_images, hr_images)
+                #edge_loss = edge_loss_fn(sr_images, hr_images)
+                #ssim_loss = ssim_loss_fn(sr_images, hr_images)
+                total_loss = lambda_mse * mse_loss + lambda_perceptual * perceptual_loss # + lambda_edge * edge_loss
                     # alpha * mse_loss + beta * perceptual_loss
 
             # backward pass and optimization
@@ -245,41 +271,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-    # Test code: check whether the dataloader is working as planned
-    # This should be in main, moving it to the bottom for readability
-
-    # def show_image_pair(hr_image, lr_image):
-    #     # Convert the tensors to numpy arrays and remove the channel dimension
-    #     if lr_image.dim() == 3:
-    #         lr_image_np = lr_image.squeeze(0).numpy()  # Remove the first dimension
-    #     else:
-    #         lr_image_np = lr_image.numpy()
-
-    #     if hr_image.dim() == 3:
-    #         hr_image_np = hr_image.squeeze(0).numpy()
-    #     else:
-    #         hr_image_np = hr_image.numpy()
-
-    #     fig, axes = plt.subplots(1, 2, figsize=(12, 6))
-    #     axes[0].imshow(lr_image_np, cmap='gray')
-    #     axes[0].set_title("Low-Resolution Image")
-    #     axes[1].imshow(hr_image_np, cmap='gray')
-    #     axes[1].set_title("High-Resolution Image")
-    #     plt.show()
-
-
-    # for i in range(5):
-    #     lr_image, hr_image = train_dataset[i]
-    #     show_image_pair(lr_image, hr_image)
-
-    # lr_image, hr_image = train_dataset[0]
-    # print("LR Image Type:", type(lr_image), "Shape:", lr_image.shape)
-    # print("HR Image Type:", type(hr_image), "Shape:", hr_image.shape)
-
-    # print("Number of LR images:", len(train_dataset.lr_images))
-    # print("Number of HR images:", len(train_dataset.hr_images))
-    # print("Dataset length:", len(train_dataset))
-
