9ef8c9d5fb9ec3cd68009316db790c44f45aa00c
hamzaehsan03 <153811806+hamzaehsan03@users.noreply.github.com>
2024-03-21 02:25:28 +0000

    Created a script to use the current model to process all the LR images into SR - This will be used as image pairs for a denoising CNN

---
+++ denoising_model_process.py
@@ -0,0 +1,50 @@
+import tensorflow as tf
+import os
+from PIL import Image
+import numpy as np
+
+def load_and_process_image(file_path):
+
+    """
+    Load an image and process it to be compatible with the model input.
+    """
+
+    img = tf.io.read_file(file_path)
+    img = tf.image.decode_png(img, channels=1) 
+    img = tf.image.convert_image_dtype(img, tf.float32)  
+    img = tf.expand_dims(img, axis=0)  
+    return img
+
+def save_image(image, file_path):
+
+    if image.ndim == 3 and image.shape[-1] == 1:
+        image = np.squeeze(image, axis=-1)
+        
+    """
+    Save an image (numpy array) to the specified file path.
+    """
+
+    image = Image.fromarray((image * 255).astype(np.uint8), 'L')  
+    image.save(file_path)
+
+model_checkpoint_path = './model_checkpoints/model_epoch_45'
+model = tf.keras.models.load_model(model_checkpoint_path, compile=False)
+
+lr_dir = './ProcessedImages/LR/Train'
+sr_dir = './SuperResolvedImages'
+os.makedirs(sr_dir, exist_ok=True)
+
+# Process each low-resolution image and save the super-resolved output
+for filename in os.listdir(lr_dir):
+    lr_image_path = os.path.join(lr_dir, filename)
+    sr_image_path = os.path.join(sr_dir, 'SR_' + filename)
+    
+    lr_image = load_and_process_image(lr_image_path)
+    
+    # Use the model to create a super-resolved image
+    sr_image = model.predict(lr_image)[0] 
+    
+    # Save the super-resolved image
+    save_image(sr_image, sr_image_path)
+
+print("All super-resolved images have been saved.")
